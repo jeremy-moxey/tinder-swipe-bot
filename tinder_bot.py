9870f3ad999c3bf4870cd4b16551f8142872c306
Sandeep Gunda <gundasandeep95@gmail.com>
2020-03-22 03:16:05 -0400

    1. Fixed FB login not found issue. 2. Replaced find_element_by_xpath by find_elemant_by_css_selector. 3. Added a popup close and safe exit at 0 likes and asked for premium.

--- tinder_bot.py
+++ tinder_bot.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from time import sleep
+from selenium.common.exceptions import NoSuchElementException
 
 from secrets import username, password
 
@@ -8,11 +9,24 @@ class TinderBot():
         self.driver = webdriver.Chrome()
 
     def login(self):
+        def check_exists_by_xpath(xpath):
+            try:
+                element = self.driver.find_element_by_xpath(xpath)
+            except NoSuchElementException:
+                return None
+            return element
+
         self.driver.get('https://tinder.com')
+
+        sleep(4)
+
+        check_more_options = check_exists_by_xpath('//button[text()="More Options"]')
+        if (check_more_options != None):
+            check_more_options.click()
 
         sleep(2)
 
-        fb_btn = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/div[2]/button')
+        fb_btn = self.driver.find_element_by_css_selector('button[aria-label="Log in with Facebook"]')
         fb_btn.click()
 
         # switch to login popup
@@ -30,18 +44,24 @@ class TinderBot():
 
         self.driver.switch_to_window(base_window)
 
-        popup_1 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+        sleep(4)
+
+        popup_1 = self.driver.find_element_by_css_selector('button[aria-label="Allow"]')
         popup_1.click()
 
-        popup_2 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+        sleep(2)
+
+        popup_2 = self.driver.find_element_by_css_selector('button[aria-label="Not interested"]')
         popup_2.click()
 
+        self.auto_swipe()
+
     def like(self):
-        like_btn = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[3]')
+        like_btn = self.driver.find_element_by_css_selector('button[aria-label="Like"]')
         like_btn.click()
 
     def dislike(self):
-        dislike_btn = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[1]')
+        dislike_btn = self.driver.find_element_by_css_selector('button[aria-label="Nope"]')
         dislike_btn.click()
 
     def auto_swipe(self):
@@ -53,15 +73,24 @@ class TinderBot():
                 try:
                     self.close_popup()
                 except Exception:
-                    self.close_match()
+                    try:
+                        self.close_match()
+                    except Exception:
+                        self.close_tinder_plus()
 
     def close_popup(self):
-        popup_3 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[2]/button[2]')
+        popup_3 = self.driver.find_element_by_xpath('//span[text()="Not interested"]')
         popup_3.click()
 
     def close_match(self):
-        match_popup = self.driver.find_element_by_xpath('//*[@id="modal-manager-canvas"]/div/div/div[1]/div/div[3]/a')
+        match_popup = self.driver.find_element_by_xpath('//a[text()="Keep Swiping"]')
         match_popup.click()
+
+    def close_tinder_plus(self):
+        tinder_plus_popup =  self.driver.find_element_by_xpath('//span[text()="No Thanks"]')
+        tinder_plus_popup.click()
+        self.driver.quit()
+        exit(0)
 
 bot = TinderBot()
 bot.login()
